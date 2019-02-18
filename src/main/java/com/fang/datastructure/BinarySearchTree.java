e6f98e284c1c7ab1e440a3ea2bb1eb67fe8f1a6e
Chi Hoang <chi_hoang@apple.com>
2019-02-18 08:18:59 -0800

    Delete a node in a BST

--- src/main/java/com/fang/datastructure/BinarySearchTree.java
+++ src/main/java/com/fang/datastructure/BinarySearchTree.java
@@ -9,42 +9,42 @@ import java.util.Deque;
  * 
  */
 public class BinarySearchTree {
-    class Node {
-        Node left, right;
+    class TreeNode {
+        TreeNode left, right;
         int data;
-        public Node(int data) {
+        public TreeNode(int data) {
             this.data = data;
         }
     }
 
-    Node root;
+    TreeNode root;
 
     public BinarySearchTree(int data) {
-        root = new Node(data);
+        root = new TreeNode(data);
     }
 
     void insert(int value) {
-        Node newNode = new Node(value);
+        TreeNode newTreeNode = new TreeNode(value);
         if (root == null) {
-            root = newNode;
+            root = newTreeNode;
             return;
         }
         
-        Node current = root;
-        Node parent = current;
+        TreeNode current = root;
+        TreeNode parent = current;
 
         while (true) {
             parent = current;
             if (value < current.data) {
                 current = current.left;
                 if (current == null) {
-                    parent.left = newNode;
+                    parent.left = newTreeNode;
                     return;
                 }
             } else {
                 current = current.right;
                 if (current == null) {
-                    parent.right = newNode;
+                    parent.right = newTreeNode;
                     return;
                 }
             }
@@ -52,7 +52,7 @@ public class BinarySearchTree {
     }
 
     boolean contains(int value) {
-        Node current = root;
+        TreeNode current = root;
         while (current != null) {
             if (value == current.data)
                 return true;
@@ -67,11 +67,39 @@ public class BinarySearchTree {
         return false;
     }
 
-    void delete(int i) {
-        // TODO: fairly complicated :)
+    TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return root;
+        
+        if (key < root.data) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.data) {
+            root.right = deleteNode(root.right, key);
+        } else { // node to delete
+            if (root.left == null && root.right == null) { // leaf
+                root = null;
+            } else if (root.left == null) { // left child null, replace
+                root = root.right;
+            } else if (root.right == null) { // right child null, replace
+                root = root.left;
+            } else { // has both left and right children
+                // find the inorder successor (go left most of the right child)
+                TreeNode curr = root.right;
+                while (curr != null && curr.left != null) {
+                    curr = curr.left;
+                }
+                // replace root with the inorder successor data
+                root.data = curr.data;
+                
+                // delete the inorder successor, guarantee no left child
+                root.right = deleteNode(root.right, curr.data);
+            }
+        }
+        
+        return root;
+        
     }
 
-    void printInOrder(Node current) {
+    void printInOrder(TreeNode current) {
         if (current == null)
             return;
 
@@ -83,15 +111,16 @@ public class BinarySearchTree {
     
     void printInOrder() {
         printInOrder(root);
+        System.out.println();
     }
 
     void inOrderWithoutRecursion() {
         if (root == null)
             return;
 
-        Deque<Node> stack = new ArrayDeque<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
         
-        Node current = root;
+        TreeNode current = root;
         while (current != null || !stack.isEmpty()) { // each level
             while (current != null) { // push to the smallest on left
                 stack.push(current);
@@ -103,7 +132,7 @@ public class BinarySearchTree {
         }
     }
 
-    void printPreOrder(Node current) {
+    void printPreOrder(TreeNode current) {
         if (current == null)
             return;
 
@@ -114,9 +143,10 @@ public class BinarySearchTree {
     
     void printPreOrder() {
         printPreOrder(root);
+        System.out.println();
     }
 
-    void printPostOrder(Node current) {
+    void printPostOrder(TreeNode current) {
         if (current == null)
             return;
         
@@ -127,14 +157,15 @@ public class BinarySearchTree {
 
     void printPostOrder() {
         printPostOrder(root);
+        System.out.println();
     }
 
     int maximum() {
         if (root == null)
             throw new RuntimeException("Root null");
         
-        Node curr = root;
-        Node last = curr;
+        TreeNode curr = root;
+        TreeNode last = curr;
 
         while (curr != null) {
             last = curr;
@@ -148,8 +179,8 @@ public class BinarySearchTree {
         if (root == null)
             throw new RuntimeException("Root null");
 
-        Node curr = root;
-        Node last = curr;
+        TreeNode curr = root;
+        TreeNode last = curr;
         
         while (curr != null) {
             last = curr;
@@ -173,16 +204,16 @@ public class BinarySearchTree {
         */
 
         tree.printInOrder(); // 10 40 50 70 80 90 
-        System.out.println();
         tree.inOrderWithoutRecursion(); // 10 40 50 70 80 90 
-        System.out.println();
         tree.printPreOrder(); // 50 10 40 80 70 90 
-        System.out.println();
         tree.printPostOrder(); // 40 10 70 90 80 50
-        System.out.println();
         System.out.println(tree.contains(40)); // true
         System.out.println(tree.contains(60)); // false
         System.out.println(tree.maximum()); // 90
         System.out.println(tree.minimum()); // 10
+        tree.deleteNode(tree.root, 50);
+        tree.printInOrder(); // 10 40 70 80 90 
+        tree.printPreOrder(); // 70 10 40 80 90 
+        tree.printPostOrder(); // 40 10 90 80 70
     }
 }
