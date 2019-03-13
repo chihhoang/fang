f3f1bbc0da087c6c62da55a5be2e771c64ccff23
Chi Hoang <chhoang102@gmail.com>
2019-03-13 08:14:27 -0700

    Fix coin change bug

--- src/main/java/com/fang/algorithm/CoinChange.java
+++ src/main/java/com/fang/algorithm/CoinChange.java
@@ -7,17 +7,18 @@ that amount of change. You can assume that there is always a
 1¢ coin.
 */
 public class CoinChange {
-    static int[] coins = new int[] {1, 5, 10};
+    static int[] coins = new int[] { 1, 5, 10 };
 
     /**
      * Brute force: Recursively check and compare all possible combinations
+     * 
      * @param n
      * @return minimum number of coins needed to make up n
      */
     static int coinChange(int n) {
         if (n == 0)
             return 0;
-        
+
         int minCoins = Integer.MAX_VALUE;
         for (int coin : coins) {
             if (coin <= n) {
@@ -35,20 +36,24 @@ public class CoinChange {
         }
         return coinChange2(n, memo);
     }
+
     /**
-     * Top-down optimization: Save the minimum numbers of coins to make 1, 2,... coins in cache
+     * Top-down optimization: Save the minimum numbers of coins to make 1, 2,...
+     * coins in cache
      */
     static int coinChange2(int n, int[] memo) {
-        if (n < 0) return -1;
+        if (n < 0)
+            return -1;
         if (n == 0)
             return 0;
-        
-        if (memo[n] > -1) return memo[n];
+
+        if (memo[n] > -1)
+            return memo[n];
 
         int minCoins = Integer.MAX_VALUE;
         for (int coin : coins) {
             if (coin <= n) {
-                minCoins = Math.min(minCoins, coinChange(n - coin));
+                minCoins = Math.min(minCoins, coinChange2(n - coin, memo));
             }
         }
 
@@ -57,7 +62,8 @@ public class CoinChange {
     }
 
     /**
-     * Bottom-up optimization: Start from n = 1,2,... and save the result in cache till n
+     * Bottom-up optimization: Start from n = 1,2,... and save the result in cache
+     * till n
      */
     static int coinChange3(int n) {
         int[] dp = new int[n + 1];
@@ -71,19 +77,19 @@ public class CoinChange {
 
             dp[i] = minCoins;
         }
-        
+
         return dp[n];
     }
 
     public static void main(String[] args) {
         long start = System.nanoTime();
         System.out.println(coinChange(50)); // 5
-        System.out.println(System.nanoTime() - start); // 40639208
+        System.out.println(System.nanoTime() - start); // 29647695
         start = System.nanoTime();
         System.out.println(coinChange2(50));
-        System.out.println(System.nanoTime() - start); // 39681409
+        System.out.println(System.nanoTime() - start); // 32392
         start = System.nanoTime();
         System.out.println(coinChange3(50));
-        System.out.println(System.nanoTime() - start); // 38528
+        System.out.println(System.nanoTime() - start); // 19501
     }
 }
